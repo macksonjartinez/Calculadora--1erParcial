2c31491c494422d384412a32b7dd89888b8d6a7c
macksonjartinez <martinezjackson2001@gmail.com>
2020-10-18 21:03:45 -0400

    Initial commit

---
+++ Calculadora-1erParcial/MainActivity.cs
@@ -0,0 +1,111 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Views;
+using System;
+                                                                                            //20197760
+namespace CalculatorAndroidTut
+{
+    [Activity(Label = "Calculadora - 2019-7760", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        private TextView calculatorText;
+
+        private string[] numbers = new string[2];
+        private string @operator;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Crear la vista de nuestro Main form.
+            SetContentView(Resource.Layout.Main);
+
+            calculatorText = FindViewById<TextView>(Resource.Id.calculator_text_view);
+        }
+
+                // Establecer interoperabilidad con Java para exportar clickeo de
+        [Java.Interop.Export("ButtonClick")]
+
+                // Establecer funcion de los botones de mostrar cuando son presionados
+        public void ButtonClick (View v)
+        {
+            Button button = (Button)v;
+            if ("0123456789.".Contains(button.Text))
+                AddDigitOrDecimalPoint(button.Text);
+            else if ("÷×+-".Contains(button.Text))
+                AddOperator(button.Text);
+            else if ("=" == button.Text)
+                Calculate();
+            else
+                Erase();
+        }
+
+                //Declaracion de decimales.
+        private void AddDigitOrDecimalPoint(string value)
+        {
+            int index = @operator == null ? 0 : 1;
+
+            if (value == "." && numbers[index].Contains("."))
+                return;
+
+            numbers[index] += value;
+
+            UpdateCalculatorText();
+        }
+                //Retortar operacion.
+        private void AddOperator(string value)
+        {
+            if (numbers[1] != null)
+            {
+                Calculate(value);
+                return;
+            }
+
+            @operator = value;
+
+            UpdateCalculatorText();
+        }
+                // Funciones de cada calculo (suma, multiplicacion, division, resta) y division de los numeros en el TBX (EVITAR TENER QUE USAR 2 TEXT BOXES)
+        private void Calculate(string newOperator = null)
+        {
+            double? result = null;
+            double? first = numbers[0] == null ? null : (double?)double.Parse(numbers[0]);
+            double? second = numbers[1] == null ? null : (double?)double.Parse(numbers[1]);
+
+            switch (@operator)
+            {
+                case "÷":
+                    result = first / second;
+                    break;
+                case "×":
+                    result = first * second;
+                    break;
+                case "+":
+                    result = first + second;
+                    break;
+                case "-":
+                    result = first - second;
+                    break;
+            }
+
+            if (result != null)
+            {
+                numbers[0] = result.ToString();
+                @operator = newOperator;
+                numbers[1] = null;
+                UpdateCalculatorText();
+            }
+        }
+                // Funcion Borrar (Boton grande de borrar)
+        private void Erase()
+        {
+            numbers[0] = numbers[1] = null;
+            @operator = null;
+            UpdateCalculatorText();
+        }
+             //Actualizacion del texto en la calculadora.
+        private void UpdateCalculatorText() => calculatorText.Text = $"{numbers[0]} {@operator} {numbers[1]}";
+    }
+}
+
---
+++ Calculadora-1erParcial/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Android.App;
+
+// Informacion autogenerada de la creacion del programa.
+[assembly: AssemblyTitle("Calculadora-1erParcial")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Calculadora-1erParcial")]
+[assembly: AssemblyCopyright("Copyright © Jackson Martinez 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
